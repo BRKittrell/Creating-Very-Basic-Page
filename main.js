1af9ceae6511448eff27888f88b8ee91ba17679d
Brandon Kittrell <brandonkittrell@Brandons-MacBook-Pro.local>
2022-12-05 16:43:16 -0500

    first commit

---
+++ main.js
@@ -0,0 +1,6 @@
+const headerdiv = document.createElement('div')
+headerdiv.innerHTML ='welcome to thunderdome'
+    document.body.append(headerdiv)
+const heading = document.createElement('h5')
+    heading.innerHTML = "Easy to forget this shit."
+    headerdiv.appendChild(heading)
